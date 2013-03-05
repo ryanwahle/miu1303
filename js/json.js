cc48e31a11272c6ccb7d0bf5b85d433947f3cb0b
Ryan Wahle <ryanwahle@fullsail.edu>
2013-03-04 21:01:02 -0700

    added json dummy data

--- js/json.js
+++ js/json.js
@@ -31,5 +31,191 @@ var jsonFakeData = {
 		"rating": "5",
 		"startingDate": "2010-08-31",
 		"description": "Navy show in Los Angeles!"
+	},
+		
+	"tvshow4": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow5": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow6": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow7": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow8": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow9": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow10": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow11": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow12": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow13": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow14": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow15": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow16": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow17": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow18": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow19": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
+	},
+
+	
+	"tvshow20": {
+		"showName": "NCIS",
+		"dayOfWeek": "Tuesday",
+		"time": "19:00",
+		"favorite": "false",
+		"rating": "4",
+		"startingDate": "2001-09-28",
+		"description": "Navy Show!"
 	}
 }
