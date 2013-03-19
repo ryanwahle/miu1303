8819900c6fbb79bfee06dc6d3aaa1117c3648c87
Ryan Wahle <ryanwahle@fullsail.edu>
2013-03-18 22:22:14 -0700

    added jqm form; replaced old main.js; made adjustments to browse by day click functions

--- gold/js/main.js
+++ gold/js/main.js
@@ -1,307 +1,149 @@
 /* Ryan Wahle */
 /* MIU 1303   */
 /* Project 3  */
+ 
+var globalBrowseByDay;
 
-function html (element)
-{
-	return document.getElementById(element);
-}
-
-function validateTVShowFormFields ()
-{
-	var errorMessagesArray = [];
-	
-	// Reset the formErrors data to be blank
-	resetFormErrors();
-	
-	// Validate the individual form fields that require validation
-	var regEx = /\S/;
-	if (!regEx.exec(html("showName").value)) {
-		errorMessagesArray.push("Please enter the Show Name");
-		html("showName").style.border = "1px solid red";
-		html("showName").value = "";
-	} 
-
-	var regEx = /^\d{1,2}:\d{2}$/;
-	if (!regEx.exec(html("time").value)) {
-		errorMessagesArray.push("Please select a Time or enter in 24HR format: HH:MM");
-		html("time").style.border = "1px solid red";
-	} 
-
-	var regEx = /^\d{4}-\d{1,2}-\d{1,2}$/;
-	if (!regEx.exec(html("startingDate").value)) {
-		errorMessagesArray.push("Please select a Starting Date or enter in format: YYYY-MM-DD");
-		html("startingDate").style.border = "1px solid red";
-	} 
-
-	var regEx = /\S/;
-	if (!regEx.exec(html("description").value)) {
-		errorMessagesArray.push("Please enter the Description");
-		html("description").style.border = "1px solid red";
-		html("description").value = "";
-	} 
-
-	// Check if any errors
-	if (errorMessagesArray.length > 0) {
-		// We have errors so don't save and show errors to user.
-		for (var i = 0; i < errorMessagesArray.length; i++) {
-			var errorTagLI = document.createElement("li");
-			errorTagLI.innerHTML = errorMessagesArray[i];
-			html("formErrors").appendChild(errorTagLI);
+/*
+$('#pageHome').on('pageinit', function(){
+	var myForm = $('#formSearch');
+	myForm.validate({
+		invalidHandler: function(form, validator) {
+			// validation code goes here?
+		},
+		submitHandler: function() {
+			//var data = myForm.serializeArray();
+			//storeData(data);
+			console.log("formSearch submitted");
 		}
-	} else {
-		// No Errors. Save the TV Show
-		saveTVShow(this.key);
-	}
-}
+	});
+});
+*/
 
-// This function removes all errors from previous actions
-// and also resets the form borders to black in case they were red.
-//
-// Why? Because I noticed if you get errors and do nothing about it,
-//		then go edit a TV Show, the errors still show on the initial
-//		edit screen.
-//
-//		So instead of putting this code in only the validate function
-//		I am also putting it in the edit function.
-function resetFormErrors ()
-{
-	html("formErrors").innerHTML = "";
-	html("showName").style.border = "1px solid black";
-	html("time").style.border = "1px solid black";
-	html("startingDate").style.border = "1px solid black";
-	html("description").style.border = "1px solid black";
-}
+$('#pageHome').on('pageinit', function () {
 
-function saveTVShow (localStorageKey)
-{
-	var myTVShow = {
-			showName: html("showName").value,
-			dayOfWeek: html("dayOfWeek").value,
-			time: html("time").value,
-			favorite: html("favorite").checked,
-			rating: html("rating").value,
-			startingDate: html("startingDate").value,
-			description: html("description").value
-	};
-	
-	// If no key, then we are adding a new TV Show.
-	// If key, then we are saving an edit.
-	if (!localStorageKey) {
-		var key = Math.floor(Math.random()*10000000001);
-		var alertMessage = "Thank you for adding a TV Show";
-	} else {
-		var key = localStorageKey;
-		var alertMessage = "Your edits to the TV Show have been saved";
-	}
-	
-	localStorage.setItem(key, JSON.stringify(myTVShow));
-	
-	// Tell the user we saved some data and reload blank page
-	alert(alertMessage);
-	window.location.reload();
-}
 
-function clearStoredData ()
-{
-	if (localStorage.length === 0) {
-		alert("There is no data to clear!");
-	} else {
-		var clearStoredDataConfirmation = confirm("Are you sure you want to clear all the data?");
-		
-		if (clearStoredDataConfirmation) {
-			localStorage.clear();
-			window.location.reload();
+// The user tried to search
+$("#formSearch").submit(function() {
+	$.mobile.changePage($("#pageSearch"));
+	return false;
+});
+
+$('.browseDayGraphic').click(function() {
+	globalBrowseByDay = this.id;
+});
+
+$("#pageBrowseShowsByDay").on("pageshow", function() {
+	//console.log(this.id);
+	$("#headerBrowseShowsByDay").text(globalBrowseByDay);
+	$("#browseLists").empty();
+	$("#browseLists").trigger("refresh");
+	
+	// Just search through JSON data since I can't access localStorage from addItem.html
+	for (var tvShowObjectKey in jsonFakeData) {
+		var fakeTVShowObject = jsonFakeData[tvShowObjectKey];
+
+		if (globalBrowseByDay == fakeTVShowObject.dayOfWeek) {
+			var tHTML = "";
+			tHTML += tHTML + "<div data-role=\"collapsible\">";
+			tHTML += "		<h4>" + fakeTVShowObject.showName + "</h4>";
+			tHTML += "		<ul data-role=\"listview\">";
+			tHTML += "			<li>Time: " + fakeTVShowObject.time + "</li>";
+			tHTML += "			<li>Day of Week: " + fakeTVShowObject.dayOfWeek + "</li>";
+			tHTML += "			<li>Favorite: " + fakeTVShowObject.favorite + "</li>";
+			tHTML += "			<li>Rating: " + fakeTVShowObject.rating + "</li>";
+			tHTML += "			<li>Starting Date: " + fakeTVShowObject.startingDate + "</li>";
+			tHTML += "			<li>Description: " + fakeTVShowObject.description + "</li>";
+			tHTML += "		</ul>";
+			tHTML += "	</div>";
+			
+			var temp = $("#browseLists");
+			temp.append(tHTML);
+			//$("#browseLists").trigger("create");
+			$("#pageBrowseShowsByDay").trigger("create");
 		}
+		
 	}
-}
+});
 
-function displayData ()
-{
-	var localStorageLength = localStorage.length;
+});
+
+// Search for a show
+$("#pageSearch").on("pageshow", function() {
+	var searchText = $("#searchTVShows").val();
+	$("#searchLists").empty();
+	$("#searchLists").trigger("refresh");
 	
-	if (localStorageLength > 0) {
-		// Hide the form, hide the display data link, and show the add tv show button
-		html("addTVShowForm").style.display = "none";
-		html("displayDataLink").style.display = "none";
-		html("addTVShowLink").style.display = "inline";
-		
-		var sectionListTVShows = document.createElement("section");
-		sectionListTVShows.setAttribute("id", "sectionListTVShows");
-		
-		// Create the new html so we can display the data
-		for (var i = 0; i < localStorageLength; i++) {
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			var myTVShow = JSON.parse(value);
-		
-			var tagTVShowList = document.createElement("ul");
-			tagTVShowList.setAttribute("id", "itemTVShow");
-			
+	// Search through JSON data since I can't access localStorage from addItem.html
+	for (var tvShowObjectKey in jsonFakeData) {
+		var fakeTVShowObject = jsonFakeData[tvShowObjectKey];
 
-			tagTVShowList.appendChild(createElementLI("Show Name: " + myTVShow.showName, "itemTVShowHeader"));
-			tagTVShowList.appendChild(getImageLI(myTVShow.dayOfWeek));
-			tagTVShowList.appendChild(createElementLI("Time: " + myTVShow.time, "itemTVShowDetail"));
-			tagTVShowList.appendChild(createElementLI("Favorite: " + myTVShow.favorite, "itemTVShowDetail"));
-			tagTVShowList.appendChild(createElementLI("Rating: " + myTVShow.rating, "itemTVShowDetail"));
-			tagTVShowList.appendChild(createElementLI("Starting Date: " + myTVShow.startingDate, "itemTVShowDetail"));
-			tagTVShowList.appendChild(createElementLI("Description: " + myTVShow.description, "itemTVShowDetail"));
+		if (searchText.toUpperCase() == fakeTVShowObject.showName.toUpperCase()) {
+			var tHTML = "";
+			tHTML += tHTML + "<div data-role=\"collapsible\">";
+			tHTML += "		<h4>" + fakeTVShowObject.showName + "</h4>";
+			tHTML += "		<ul data-role=\"listview\">";
+			tHTML += "			<li>Time: " + fakeTVShowObject.time + "</li>";
+			tHTML += "			<li>Day of Week: " + fakeTVShowObject.dayOfWeek + "</li>";
+			tHTML += "			<li>Favorite: " + fakeTVShowObject.favorite + "</li>";
+			tHTML += "			<li>Rating: " + fakeTVShowObject.rating + "</li>";
+			tHTML += "			<li>Starting Date: " + fakeTVShowObject.startingDate + "</li>";
+			tHTML += "			<li>Description: " + fakeTVShowObject.description + "</li>";
+			tHTML += "		</ul>";
+			tHTML += "	</div>";
 			
-			var linksLI = createElementLI("");
-			linksLI.appendChild(createDeleteLink("Delete TV Show", key));
-			linksLI.appendChild(createEditLink("Edit TV Show", key));				
-
-			tagTVShowList.appendChild(linksLI);
-						
-			// Put it all the added HTML to the page!
-			sectionListTVShows.appendChild(tagTVShowList);
-			
-			html("mainSection").appendChild(sectionListTVShows);
+			var temp = $("#searchLists");
+			temp.append(tHTML);
+			//$("#searchLists").trigger("create");
+			$("#pageSearch").trigger("create");
 		}
-	} else {
-		var addJSONDataConfirmation = confirm("There are currently no TV Shows to display. Would you like to load some pre-defined data?");
-	
-		if (addJSONDataConfirmation) {
-			for (var tvShowObjectKey in jsonFakeData) {
-				var fakeTVShowObject = jsonFakeData[tvShowObjectKey];
-				var key = Math.floor(Math.random() * 10000000001);
-				localStorage.setItem(key, JSON.stringify(fakeTVShowObject));
-			}
-			
-			alert ("The pre-defined data has been loaded.");
-			
-			displayData();
+		
+	}
+
+});
+
+/*
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#formId');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
 		}
-	}
-}
-
-function getImageLI (dayOfWeek)
-{
-	var imageTag = document.createElement("img");
-	imageTag.setAttribute("src", "img/" + dayOfWeek + ".png");
-
-	var imageLI = createElementLI("", "itemTVShowDetail");
-	imageLI.appendChild(imageTag);
+	});
 	
-	return imageLI;
-}
+	//any other code needed for addItem page goes here
+	
+});
 
-function deleteTVShow ()
-{
-	var deleteConfirmation = confirm("Are you sure you want to delete that TV Show?");
-	
-	if (deleteConfirmation) {
-		localStorage.removeItem(this.key);
-		window.location.reload();
-	}
-}
 
-function editTVShow ()
-{
-	// Reset any form validation errors in case user went from not fixing
-	// errors to editing a TV Show
-	resetFormErrors();
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-	// The rest of the function
-	var myTVShow = JSON.parse(localStorage.getItem(this.key));
-	
-	html("showName").value = myTVShow.showName;
-	html("dayOfWeek").value = myTVShow.dayOfWeek;
-	html("time").value = myTVShow.time;
-	html("rating").value = myTVShow.rating;
-	html("startingDate").value = myTVShow.startingDate;
-	html("description").value = myTVShow.description;
-	
-	if (myTVShow.favorite) {
-		html("favorite").setAttribute("checked", "checked");
-	}
-	
-	// Setup the save button for editing the tv show
-	html("addTVShowButton").value = "Save";
-	html("addTVShowButton").key = this.key;
-	
-	// Hide the listing and show the form with updated legend
-	html("actionDescription").innerHTML = "Editing . . .";
-	html("actionDescription").style.backgroundColor = "yellow";
-	html("sectionListTVShows").style.display = "none";
-	html("addTVShowForm").style.display = "";
-	
-}
+var autofillData = function (){
+	 
+};
 
-function createEditLink (text, key)
-{
-	var tag = document.createElement("a");
-	tag.setAttribute("id", "showLink");
-	tag.innerHTML = text;
-	tag.href = "#";
-	tag.key = key;
-	tag.addEventListener("click", editTVShow);
-	return tag;
-}
+var getData = function(){
 
-function createDeleteLink (text, key)
-{
-	var tag = document.createElement("a");
-	tag.setAttribute("id", "showLink");
-	tag.innerHTML = text;
-	tag.href = "#";
-	tag.key = key;	
-	tag.addEventListener("click", deleteTVShow);
-	return tag;
-}
+};
 
-function createElementLI (text, tagID)
-{
-	var tag = document.createElement("li");
-	tag.innerHTML = text;
-	tag.setAttribute("id", tagID);
+var storeData = function(data){
 	
-	return tag;
-}
+}; 
 
-function addDaysOfWeekSelectTag ()
-{
-	var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	
-	var selectionLabel = html("dayOfWeekSelection");
-	var selectTag = document.createElement("select");
-	selectTag.setAttribute("id", "dayOfWeek");
-	
-	for (var i = 0; i < daysOfWeek.length; i++) {
-		var optionTag = document.createElement("option");
-		optionTag.value = daysOfWeek[i];
-		optionTag.innerHTML = daysOfWeek[i];
-		selectTag.appendChild(optionTag);
-	}
-	
-	selectionLabel.appendChild(selectTag);
-	
-}
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
 
-function cancelAddTVShow ()
-{
-	location.href = "index.html";
-}
-
-// This is our starting point (after the DOM loads)
-function DOMLoaded ()
-{
-	// Add our select tag
-	addDaysOfWeekSelectTag();
-
-	// Add some event listeners
-	var addTVShowButton = html("addTVShowButton");
-	var cancelAddTVShowButton = html("cancelAddTVShowButton");
-	
-	var clearStoredDataLink = html("clearStoredDataLink");
-	var displayDataLink = html("displayDataLink");
-
-	addTVShowButton.addEventListener("click", validateTVShowFormFields);
-	cancelAddTVShowButton.addEventListener("click", cancelAddTVShow);
-	
-	clearStoredDataLink.addEventListener("click", clearStoredData);
-	displayDataLink.addEventListener("click", displayData);
-}
-
-window.addEventListener("DOMContentLoaded", DOMLoaded);
+};
+*/
 
